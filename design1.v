1b4cb553a98ad14773a43152beb69154aa708e93
gautamdev17 <gautamd.nxt@gmail.com>
2025-12-23 00:11:52 +0530

    got a software perspective to the problem, going to sleep tired, 12:11AM, 22-12-25

--- design1.v
+++ design1.v
@@ -1,6 +1,7 @@
 module access #(parameter width,parameter depth)(input [width-1:0][depth-1:0]mat,output reg [$clog2(width*depth):0]count); 
 // width is size of each row, depth is no. of rows
 // i hv synthesized fifo's, b4 so naming is similar to them
+// got a software perspective to the problem
     integer i,j;
     always @(*) begin
         count=0;
@@ -8,17 +9,28 @@ module access #(parameter width,parameter depth)(input [width-1:0][depth-1:0]mat
             for(j=0;j<width;j=j+1)begin
                 if(mat[i][j] == '@') begin
                     if(j==0) begin
-                        if(i==0) begin
+                        if(i==0) begin // corner case
 
                         end
-                        else if(i==depth-1) begin
+                        else if(i==depth-1) begin // corner case
 
                         end
-                        else begin
+                        else begin // edge case
 
                         end
                     end
-                    else if((j==0 | j==width-1) & i==0) begin
+                    else if(i==0) begin
+                        if(j==0) begin // corner case
+
+                        end
+                        else if(j==depth-1) begin // corner case
+
+                        end
+                        else begin // edge cases
+
+                        end
+                    end
+                    else begin //normal case
 
                     end
                 end
