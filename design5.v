a0da205be8e9a00bc443094be5b66878eb265d4c
gautamdev17 <gautamd.nxt@gmail.com>
2025-12-26 15:20:20 +0530

    trialing part2

--- design5.v
+++ design5.v
@@ -2,7 +2,7 @@
 // we can invoke hardware unit in previous part,basically we can first create instance of part 1 module and reuse it
 // maybe with a bit of modification
 // since this is part has a sequence, i think modelling this into a sequential circuit is the best way
-module exhaustive_access #(parameter WIDTH = 16,parameter DEPTH = 16)(input [WIDTH-1:0]mat_init[DEPTH-1:0],input clk,input reset//1=paper,0=nothin
+module exhaustive_access #(parameter WIDTH = 16,parameter DEPTH = 16)(input [WIDTH-1:0]mat_init[DEPTH-1:0],input clk,input reset,//1=paper,0=nothin
 output reg [$clog2(WIDTH*DEPTH+1)-1:0]count,output reg done);//added flag regs
     reg [WIDTH-1:0]mat_out[DEPTH-1:0];
     reg [WIDTH-1:0]mat_in[DEPTH-1:0];
@@ -10,32 +10,27 @@ output reg [$clog2(WIDTH*DEPTH+1)-1:0]count,output reg done);//added flag regs
     reg started;
     wire [$clog2(WIDTH*DEPTH+1)-1:0]removed;
 
-    remove_accessible inst1 #(WIDTH,DEPTH)(mat_in,mat_out,removed);//instantiation this will do the math for uss
+    remove_accessible #(.WIDTH(WIDTH),.DEPTH(DEPTH)) inst1 (mat_in,mat_out,removed);//instantiation this will do the math for uss
     // now i haveto build a top level software approach then
      //go into the hardware low level design
     integer i;
     always @(posedge clk) begin
-    if(reset) begin
-        for(i=0;i<DEPTH;i=i+1)
+    if (reset) begin
+        for (i=0;i<DEPTH;i=i+1)
             mat_in[i] <= mat_init[i];
         count <= 0;
         done  <= 0;
-        removed_q <= 0;
-        started <= 0;
     end
-    else if(!done) begin
-    removed_q <= removed;
-
-    if(started && removed_q == 0) begin
-        done <= 1'b1;
+    else if (!done) begin
+        if (removed != 0) begin
+            count <= count + removed;
+            for (i=0;i<DEPTH;i=i+1)
+                mat_in[i] <= mat_out[i];
+        end
+        else begin
+            done <= 1'b1;
+        end
     end
-    else begin
-        started <= 1'b1;
-        count <= count + removed_q;
-        for(i=0;i<DEPTH;i=i+1)
-            mat_in[i] <= mat_out[i];
-    end
-end
 end
 endmodule
 
